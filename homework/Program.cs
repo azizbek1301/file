790867befe210b807008d475c7edfcc9493948bb
azizbek1301 <azizbekrakhmatullayev1317@gmail.com>
2023-07-03 07:32:51 +0500

    Add project files.

---
+++ homework/Program.cs
@@ -0,0 +1,35 @@
+﻿
+
+namespace homework
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string path = "hello";
+
+            ForFile forFile = new ForFile();
+
+            forFile.CreateOrWriteFile(path, "Qande");
+        }
+    }
+    class ForFile
+    {
+        public void OpenFile(string path)
+        {
+            for (int i = 1; i <= 10; i++)
+            {
+                File.Open(path + $"{i}.txt", FileMode.CreateNew);
+            }
+            Console.WriteLine("File yaratildi");
+        }
+        public void CreateOrWriteFile(string path, string text)
+        {
+            for (int i = 1; i <= 10; i++)
+            {
+                File.WriteAllText(path + $"{i}.txt", text);
+            }
+            Console.WriteLine("Filega yozildi");
+        }
+    }
+}
